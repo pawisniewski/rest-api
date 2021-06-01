07c4451f3ac24e0a0e891369fe569e059098bb8e
Pawel Wisniewski <pawel_wisniewski@outlook.com>
2021-06-01 13:21:57 +0200

    Move db to atlas

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use((req, res) => {
 });
 
 // connects our backend code with the database
-mongoose.connect('mongodb://localhost:27017/NewWaveDB', { useNewUrlParser: true });
+mongoose.connect('mongodb+srv://pawel_wisniewski:KYrpqNCsjo8rbUQU@cluster1.3rvv0.mongodb.net/NewWaveDB?retryWrites=true&w=majority', { useNewUrlParser: true }, { useUnifiedTopology: true });
 const db = mongoose.connection;
 
 db.once('open', () => {
